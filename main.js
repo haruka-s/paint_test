75ece777e7c72982ed760ebd43152286d84ed833
haruka sasaki <haruka_s@Nyarlathotep.local>
2017-05-12 10:42:03 +0900

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',function(){
+	var canvas = document.getElementById('id_canvas');
+	var context = canvas.getContext('2d');
+	context.fillstyle = 'blue';
+
+	canvas.addEventListener('mousemove', function(eve){
+		console.log(eve);
+		context.fillRect(eve.layerX,eve.layerY,10,10);
+	}, false);
+
+})
