22bea88c987d8fc7f9c82d39b138d674713a81fe
haruka sasaki <haruka_s@Nyarlathotep.local>
2017-05-12 12:04:48 +0900

    イベントの変更

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ window.addEventListener('load',function(){
 	var canvas = document.getElementById('id_canvas');
 	var context = canvas.getContext('2d');
 	context.fillstyle = 'blue';
+	var flag = false;
+	var x = 0;
+	var y = 0;
 
 	canvas.addEventListener('mousemove', function(eve){
-		console.log(eve);
-		context.fillRect(eve.layerX,eve.layerY,10,10);
+		if(flag === true){
+			console.log(eve);
+			var rect = eve.target.getBoundingClientRect();
+			x = eve.clientX - rect.left;
+			y = eve.clientY - rect.top;
+			context.fillRect(x,y,10,10);
+		}
+//		context.fillRect(eve.layerX,eve.layerY,10,10);
+	}, false);
+
+	canvas.addEventListener('mousedown', function(eve){
+		flag = true;
+//		var rect = eve.target.getBoundingClientRect();
+//		x = eve.clientX - rect.left;
+//		y = eve.clientY - rect.top;
+
+	}, false);
+
+	canvas.addEventListener('mouseup', function(eve) {
+		flag = false;
 	}, false);
 
 	canvas.width=window.innerWidth;
